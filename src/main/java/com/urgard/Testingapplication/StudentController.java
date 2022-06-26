ae7b965618a127e424173a2d957e2f6cfd2e5516
utgard <daniel.strbak@seznam.cz>
2022-06-26 20:39:03 +0200

    Add StudentController and add functionality

---
+++ src/main/java/com/urgard/Testingapplication/StudentController.java
@@ -0,0 +1,30 @@
+package com.urgard.Testingapplication;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students/")
+@AllArgsConstructor
+public class StudentController {
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addStudent(@Valid @RequestBody Student student) {
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping("{studentId}")
+    public void deleteStudent(@PathVariable("{studentId}") Long studentId) { //is @PathVariable necessary? -> test it
+        studentService.deleteStudent(studentId);
+    }
+
+}
