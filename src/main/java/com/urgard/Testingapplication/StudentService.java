664108904b09ce31047ec4195422255598bb5cf1
utgard <daniel.strbak@seznam.cz>
2022-06-26 20:38:37 +0200

    Add StudentService and add functionality

---
+++ src/main/java/com/urgard/Testingapplication/StudentService.java
@@ -0,0 +1,30 @@
+package com.urgard.Testingapplication;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StudentService {
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student) {
+        var emailExists = studentRepository.selectExistsEmail(student.getEmail());
+        if (emailExists)
+            throw new IllegalArgumentException("Email " + student.getEmail() + "already taken");
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        var studentExists = studentRepository.existsById(id);
+        if (!studentExists)
+            throw new NullPointerException("Student with id " + id +"does not exists");
+        studentRepository.deleteById(id);
+    }
+}
