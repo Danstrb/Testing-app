9d529aae7c0fb1fdbc0a4afab766e48b14d599b7
utgard <daniel.strbak@seznam.cz>
2022-06-26 23:20:29 +0200

    Organize the directory - unimportant

--- src/main/java/com/urgard/Testingapplication/TestApp.java
+++ src/main/java/com/urgard/Testingapplication/TestApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(TestingApplication.class, args);
 	}
 
